cdc97923bde100692fcb76249c3d4375199f4f49
Raihan Kulmagambetova <r.kulmagambetova@framework.team>
2020-10-12 19:47:50 +0400

    create icons

---
+++ src/components/icons/IconClose/IconClose.vue
@@ -0,0 +1,18 @@
+<template>
+  <IconBase>
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path
+      d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
+    />
+  </IconBase>
+</template>
+
+<script>
+import IconBase from '../IconsBase';
+
+export default {
+  components: {
+    IconBase,
+  },
+};
+</script>
