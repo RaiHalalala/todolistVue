cbe03ae98d5a06d29b9486d146719d545b20a79a
Raihan Kulmagambetova <r.kulmagambetova@framework.team>
2020-10-13 18:29:56 +0400

    add components

--- src/components/Task/Task.vue
+++ src/components/Task/Task.vue
@@ -1,31 +1,60 @@
 <template>
   <li class="Task list-group-item d-flex">
-    <div>
-      {{task.title}}
+    <Button isIcon  @click="onRemuveTask" class="Task__close">
+      <IconClose/>
+    </Button>
+    <div >
+      <div class="Task__content-left">
+        <input
+          type="checkbox"
+          id="checkbox"
+          v-model="checked"
+          @change="onChangeTask('completed', !task.completed)">
+        <label for="checkbox"></label>
+        <div>
+          {{task.title}}
+        </div>
+      </div>
+      <div>
+        {{task.dateOfCreat}}
+      </div>
     </div>
-    <div class="d-flex">
-      <Button isIcon  @click="onRemuveTask">
-        <IconClose/>
-      </Button>
-      {{task.dateOfCreat}}
-    </div>
+    <Button isIcon @click="onChangeTask('favorite', !task.favorite)" >
+      <IconFavorite :active="task.favorite"/>
+    </Button>
   </li>
 </template>
 
 <script>
 import Button from '../Button';
 import IconClose from '../icons/IconClose';
+import IconFavorite from '../icons/IconFavorite';
 
 export default {
+  data() {
+    return {
+      checked: this.task.completed,
+    };
+  },
   props: {
     task: Object,
   },
   components: {
-    Button, IconClose,
+    Button, IconClose, IconFavorite,
   },
   methods: {
     onRemuveTask() {
       this.$store.dispatch('fetchRemuveTask', this.task.id);
+    },
+    onChangeTask(path, value) {
+      const data = {
+        [path]: value,
+      };
+      const param = {
+        data,
+        idTask: this.task.id,
+      };
+      this.$store.dispatch('fetchUpdateTask', param);
     },
   },
 };
@@ -34,5 +63,16 @@ export default {
 <style lang="scss">
 .Task {
   justify-content: space-between;
+  position: relative;
+
+  &__close {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+  &__content-left {
+    display: flex;
+    align-items: center;
+  }
 }
 </style>
