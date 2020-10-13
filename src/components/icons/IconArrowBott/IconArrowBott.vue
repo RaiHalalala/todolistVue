40759e5de3ec001c81219804f9b63e03a1e83fac
Raihan Kulmagambetova <r.kulmagambetova@framework.team>
2020-10-13 18:28:52 +0400

    create component

---
+++ src/components/icons/IconArrowBott/IconArrowBott.vue
@@ -0,0 +1,19 @@
+<template>
+  <IconBase :size="30+'px'">
+    <path
+      d="M0 0h24v24H0z"
+      fill="none"/>
+      <path
+        d="M7 10l5 5 5-5z"/>
+  </IconBase>
+</template>
+
+<script>
+import IconBase from '../IconsBase';
+
+export default {
+  components: {
+    IconBase,
+  },
+};
+</script>
