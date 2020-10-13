39aeeb6d22eea5beab99aaea3a5bfb62db3d425c
Raihan Kulmagambetova <r.kulmagambetova@framework.team>
2020-10-13 18:29:33 +0400

    improved size icon

--- src/components/icons/IconsBase/IconBase.vue
+++ src/components/icons/IconsBase/IconBase.vue
@@ -2,14 +2,10 @@
     <svg xmlns="http://www.w3.org/2000/svg"
     :width="width"
     :height="height"
-    viewBox="0 0 18 18"
+    :viewBox="viewBox"
     :aria-labelledby="iconName"
     role="presentation"
   >
-    <title
-      :id="iconName"
-      lang="en"
-    >{{iconName}} icon</title>
     <g :fill="iconColor">
       <slot />
     </g>
@@ -20,15 +16,28 @@
 export default {
   data() {
     return {
-      width: '24px',
-      height: '24px',
+      width: this.size || '24px',
+      height: this.size || '24px',
       iconName: 'icon',
-      iconColor: '#000000',
     };
+  },
+  computed: {
+    iconColor() {
+      const color = this.favorite && this.active ? '#ffd700' : '#CCCCCC';
+      return color;
+    },
+    viewBox() {
+      const sizeView = this?.size?.replace('px', '');
+      const view = this.size ? `0 0 ${sizeView} ${sizeView}` : '0 0 24 24';
+      return view;
+    },
   },
   props: {
     largeIcon: Boolean,
     smallIcon: Boolean,
+    active: Boolean,
+    favorite: Boolean,
+    size: String,
   },
 };
 </script>
